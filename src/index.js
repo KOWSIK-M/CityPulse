2f4ab4415823054948faafb2f39c581c505d283c
MPVLKowsik <2200030358@kluniversity.in>
2024-10-08 08:30:10 +0530

    edited index.js

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ function Website() {
     <div>
         <Routes basename="/Citizo">
         <Route path="/Citizo" element={<App/>} />
-        <Route path="Citizo/userlogin" element={<Userlogin/>} />
-        <Route path="Citizo/adminlogin" element={<Adminlogin/>} />
-        <Route path="Citizo/contact" element={<Contact/>}/>
-        <Route path="Citizo/about" element={<About/>}/>
-        <Route path="Citizo/dashboard" element={<Dashboard/>}/>
-        <Route path="Citizo/userdashboard" element={<Userdashboard/>}/>
-        <Route path="Citizo/leafmaps" element={<LeafMaps/>}/>
+        <Route path="/userlogin" element={<Userlogin/>} />
+        <Route path="/adminlogin" element={<Adminlogin/>} />
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/userdashboard" element={<Userdashboard/>}/>
+        <Route path="/leafmaps" element={<LeafMaps/>}/>
         </Routes>
     </div>
     </BrowserRouter>
